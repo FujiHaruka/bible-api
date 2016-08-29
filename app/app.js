636f9824f03061a7c903d8c2169976e21c871fcb
FujiHaruka <fujiharuka@realglobe.jp>
2016-08-29 16:42:38 +0900

    Write API Reference

--- app/app.js
+++ app/app.js
@@ -7,7 +7,7 @@ let app = module.exports = koa()
 app.use(route.get('/api/:book/:chapter/:verse', controller.fetchOne))
 app.use(route.get('/api/:book/from/:fromChapter/:fromVerse/to/:toChapter/:toVerse', controller.fetchRange))
 
-if (module.parent) {
+if (!module.parent) {
   app.use(logger())
   app.listen(3000)
 }
