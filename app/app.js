d5e7dfaabbf40bc3ce78fe22e1b55da65c0e3717
FujiHaruka <fujiharuka@realglobe.jp>
2016-09-19 21:05:02 +0900

    Configure CORS

--- app/app.js
+++ app/app.js
@@ -1,10 +1,14 @@
 const koa = require('koa')
 const logger = require('koa-logger')
 const route = require('koa-route')
+const cors = require('koa-cors')
 const controller = require('./helper/controller')
 const { APP_PORT, URL_PREFIX } = require('../env')
 let app = module.exports = koa()
 
+app.use(cors({
+  origin: '*'
+}))
 app.use(route.get(URL_PREFIX + '/:book/:chapter/:verse', controller.fetchOne))
 app.use(route.get(URL_PREFIX + '/:book/from/:fromChapter/:fromVerse/to/:toChapter/:toVerse', controller.fetchRange))
 
